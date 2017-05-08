0d7c9aa4d485c69500e7bf4f36767b3fb7461db1
Deni Shakh <admin@MacBook-dl.local>
2017-05-08 17:33:39 -0400

    final release

--- main.py
+++ main.py
@@ -1,41 +1,108 @@
+import os,csv
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+import struct
 from PIL import Image
-import sys, csv
-from resizeimage import resizeimage
-import math
+import scipy
+import scipy.misc
+import scipy.cluster
+import numpy as np
+import base64
+import cStringIO
 
-class Palitra(file):
-	
-	def rgb_to_hex(rgb):
-		return '%02x%02x%02x' % rgb
+UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__))
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'JPG', 'PNG'])
 
-	def check(val):
-		final = set()
-		with open('AcceptedColors.csv') as csvfile:
-		    color = csv.reader(csvfile, delimiter=',', quotechar='|')
-		    next(color)
-		    for row in color:
-		        name = (row[0])
-		        hexdec = (row[1])
-		        for col in val:
-		        	if col == hexdec.lower():
-			        	final.add(col)
-		print final
-	
-	def __init__(self, file):
+def getcol(codes, NUM_CLUSTERS):
+    s = set()
+    for c in range(0, NUM_CLUSTERS):
+        peak = codes[c]
+        peak = peak.astype(int)
+        colour = ''.join(format(c, '02x') for c in peak)
+        final = ('#%s' % colour)
+        s.add(final)
+    return s
 
-		im = Image.open(file)
-		#im = resizeimage.resize_cover(im, [512, 512])
-		pix = im.load()
-		w, h = im.size # breaking the tuple into width and height in px
-		im = resizeimage.resize_cover(im, [150, 150])
-		s = set() #creating a set
-		#print im.size # Get the width and hight of the image for iterating over
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-		for x in range(0, w):
-			for y in range(0, h):
-				hex = rgb_to_hex(pix[x,y]) #converting set to hex and storing it
-				s.add(hex)
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+NUM_CLUSTERS = 7
+im = Image.open('static/images/image.jpg')
+buffer = cStringIO.StringIO()
+im.save(buffer, format="JPEG")
+img_str = base64.b64encode(buffer.getvalue())
+uploadingimage = img_str
 
-		s = sorted(s) #sorting out the s set
+im = im.resize((150, 150))
 
-		check(s)
+ar = scipy.misc.fromimage(im)
+shape = ar.shape
+ar = ar.reshape(scipy.product(shape[:2]), shape[2])
+codes, dist = scipy.cluster.vq.kmeans(ar.astype(float), NUM_CLUSTERS)
+
+vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
+s = set()
+s = counts
+
+x = set()
+x = getcol(codes, NUM_CLUSTERS)
+
+
+
+
+def GetnewColors():
+    NUM_CLUSTERS = 7
+    im = Image.open('static/images/image.jpg')
+    im = im.resize((150, 150))
+
+    ar = scipy.misc.fromimage(im)
+    shape = ar.shape
+    ar = ar.reshape(scipy.product(shape[:2]), shape[2])
+    codes, dist = scipy.cluster.vq.kmeans(ar.astype(float), NUM_CLUSTERS)
+
+    vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+    counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
+    s = set()
+    s = counts
+    x = set()
+    x = getcol(codes, NUM_CLUSTERS)
+    return x
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file(x=x, im=uploadingimage):
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            im = Image.open(file.filename)
+            im.save('static/images/image.jpg')
+            pix = im.load()
+            buffer = cStringIO.StringIO()
+            im.save(buffer, format="JPEG")
+            img_str = base64.b64encode(buffer.getvalue())
+            uploadingimage = img_str
+            im = im.resize((150, 150))
+            x = GetnewColors()
+
+
+        return render_template('index.html', x = x, im=uploadingimage)
+    x = GetnewColors()
+    return render_template('index.html', x = x, im=uploadingimage)
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
